274ac17a3346b47d7e07b7cb66ad3f43eb0a6d88
TatianeAlves13 <alves1395.thatyanne@gmail.com>
2025-04-07 21:53:00 -0300

    atualização do css

--- app.js
+++ app.js
@@ -1,2 +1,90 @@
 console.log("Anne")
 console.log(39)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function fnDestaque1Original(){
+    document.getElementById("destaque1").src
+}
+
+
+
+function fnRegistroContato(){
+    let nome = document.getElementById("nome").value
+    let email = document.getElementById("email").value
+    let mensagem = document.getElementById("mensagem").value
+    let informacoesCompleta = nome + " " + email + " " + mensagem 
+    document.getElementById("informacoes").innerHTML = informacoesCompleta
+
+    //limpar os campos
+    document.getElementById("nome").value = ""
+    document.getElementById("email").value = ""
+    document.getElementById("mensagem").value = ""
+}
